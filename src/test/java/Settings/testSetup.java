c498333cb6789bbeaaa03208c66c7cd537f83241
Bien Bryan Laririt <66415917+git-bryanlaririt@users.noreply.github.com>
2025-02-18 14:37:26 +0800

    Updated testSetup.java
    Added missing import for Selenium Webdriver Manager

--- src/test/java/Settings/testSetup.java
+++ src/test/java/Settings/testSetup.java
@@ -1,6 +1,7 @@
 package Settings;
 
 import org.openqa.selenium.WebDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
